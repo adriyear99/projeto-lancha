88c679d5638ac8f5c31d9e0c8fdc38f392813d96
Adriano Tavares Andrade <adriano.tavares.andrade@gmail.com>
2022-08-27 04:56:23 -0300

    Primeiro Componente

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Components
+import Home from './src/Home';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá Mundo</Text>
+      <Text>você caiu no phishing</Text>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function Home() {
+    return (
+        <Text>Primeiro Componente</Text>
+    );
+}
