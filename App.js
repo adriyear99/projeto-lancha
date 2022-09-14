eb2adc6efe2729756d7a297b20cf5c10632241c4
Adriano Tavares Andrade <adriano.tavares.andrade@gmail.com>
2022-09-14 17:01:18 -0300

    Remoção da header inicial

--- App.js
+++ App.js
@@ -17,24 +17,12 @@ export default function App() {
 
   // Navegador de páginas
   const Stack = createNativeStackNavigator()
-  // const navigationRef = useNavigationContainerRef();
-  // console.log("Teste:",navigationRef)
-  // const routeNameRef = useRef();
-  // console.log(navigationRef.getCurrentRoute().name)
-  // console.log(routeNameRef)
-
-  // const { navigate } = this.props.navigation
-  // console.log(navigate)
-
-  // const route = useRoute()
-
 
   // Renderização dos componentes
   return (
     <NavigationContainer>
-      {/* screenOptions={{ headerShown: (route.name != "Tela Inicial") }} */}
       <Stack.Navigator initialRouteName="Tela Inicial">
-        <Stack.Screen name="Tela Inicial" component={CadastroLogin}/>
+        <Stack.Screen name="Tela Inicial" component={CadastroLogin} options={{headerShown: false}}/>
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Cadastro" component={Cadastro}/>
