0049f36754b124e3ad69704647eefe3e9232821f
fabio <te_pe_@hotmail.com>
2022-10-03 11:42:08 -0300

    Google auth funciona mas retorna erro de host

--- src/components/LoginComp.js
+++ src/components/LoginComp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {GoogleLogin} from 'react-google-login';
 
 //Login google
-const clientId = '192988181548-p5adg3se24ecfkcrv6bdtnfkjj9l040j.apps.googleusercontent.com';
+const clientId = '192988181548-40l8e2h22lc3fsog7augfocd5mnc8c06.apps.googleusercontent.com';
 
 function Login() {
     const onSucess = (res) => {
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {GoogleLogout} from 'react-google-login';
 
 //Login google
-const clientId = '192988181548-p5adg3se24ecfkcrv6bdtnfkjj9l040j.apps.googleusercontent.com';
+const clientId = '192988181548-40l8e2h22lc3fsog7augfocd5mnc8c06.apps.googleusercontent.com';
 
 function Logout() {
     const onSucess = () => {
