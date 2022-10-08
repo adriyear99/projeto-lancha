1845fa42db673cdf8d722ebcd947052b9db78ab2
adriyear99 <adriano.tavares.andrade@gmail.com>
2022-10-07 22:19:57 -0300

    Implementação da tela de configurações

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,7 +1,8 @@
 // Utilidades
-import { StyleSheet,Text,View } from 'react-native'
+import { StyleSheet,Text,View,TouchableOpacity } from 'react-native'
 
 // Componentes
+import CustomButton from '../components/CustomButton'
 
 // Expo Icons
 
@@ -10,7 +11,26 @@ export default function Settings({navigation}) {
 
     return (
         <View style={styles.container}>
-            <Text>Settings</Text>
+            <CustomButton 
+                text='Editar Perfil' 
+                onPress={console.log('teste')}
+                style={{ height:60, width:300, backgroundColor:'#4B7E94' }}
+            />
+            <CustomButton 
+                text='Privacidade e Segurança' 
+                onPress={console.log('teste')}
+                style={{ height:60, width:300, backgroundColor:'#4B7E94' }}
+            />
+            <CustomButton 
+                text='Acessibilidade' 
+                onPress={console.log('teste')}
+                style={{ height:60, width:300, backgroundColor:'#4B7E94' }}
+            />
+            <CustomButton 
+                text='Fazer Logout' 
+                onPress={console.log('teste')}
+                style={{ height:60, width:300, backgroundColor:'#4B7E94' }}
+            />
         </View>
     )
 }
@@ -21,8 +41,10 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor:'#fff',
         padding:20,
-        // alignItems:'center',
-        // justifyContent:'center',
+        alignItems:'center',
+        justifyContent:'center',
+        // borderWidth:2,
+        // borderColor:'red'
     },
 
 })
